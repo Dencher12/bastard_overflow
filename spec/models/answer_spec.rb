c2b4f70b60195e41cc649ea46f6395aacd73b031
Den <deischerry19@gmail.com>
2020-01-01 15:15:14 +0300

    Добавил валидацию тела ответа

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :body }
+
+  it 'the answer has one question' do
+    q = FactoryBot.build(:question)
+    a = FactoryBot.build(:answer)
+    a.question = q
+    expect(a.question).to eq(q)
+  end 
 end
