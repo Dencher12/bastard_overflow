9fa68ed48f40d30b1d8dbf729c335a96712736a7
Den <denischerry19@gmail.com>
2020-01-12 15:56:55 +0300

    Покрыл тестами создание вопроса и просмотр списка вопросов, а также пофиксил спеку регистрации

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,15 +5,20 @@ feature 'User sign up', %q{
   As an user
   I want to be able to sign up
 } do
-  given(:user) { create(:user) }
-
   scenario "User tries to sign up and email isn't in the db" do
-    click_on 'Sign up'
+    user = build(:user)
+
+    sign_up(user)
+
     expect(page).to have_content 'Welcome! You have signed up successfully.'
     expect(current_path).to eq root_path
   end
 
   scenario 'User tries to sign up and email already exists in the db' do
+    user = create(:user)
+
+    sign_up(user)
+
     click_on 'Sign up'
     expect(page).to have_content 'Email has already been taken'
   end
