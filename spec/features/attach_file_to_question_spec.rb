b408a24041eaf50d0bf1be9eeb0477dc853d57c4
Den <denischerry19@gmail.com>
2020-01-28 00:30:51 +0300

    Добавил прикрепление файла к вопросу

---
+++ spec/features/attach_file_to_question_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature 'Attach a file to a question', %q{
+  In order do supplement my question
+  As an author of a question
+  I want to be able to attach a file
+} do
+
+  given(:user) { create(:user) }
+
+  background do
+    sign_in(user)
+    visit new_question_path
+  end
+
+  scenario 'Auth user try to attach file when asks question' do
+    fill_in 'Title', with: '?'
+    fill_in 'Body', with: '???'
+
+    attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    click_on 'Create Question'
+    expect(page).to have_link 'rails_helper.rb'
+  end
+end
